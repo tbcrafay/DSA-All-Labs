8006e60554847f6acaed6f9b9a409b8ce937005d
Abdul Rafay <140069786+tbcrafay@users.noreply.github.com>
2024-11-10 18:13:57 +0500

    Create task5.java

---
+++ Lab1/Lab_tasks/task5.java
@@ -0,0 +1,22 @@
+public class MinMaxValues {
+    public static void main(String[] args) {
+        // Integer
+        int minInt = Integer.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+
+        // Float
+        float minFloat = Float.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+
+        // Double
+        double minDouble = Double.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value: " + minInt);
+        System.out.println("Integer Maximum Value: " + maxInt);
+        System.out.println("Float Minimum Value: " + minFloat);
+        System.out.println("Float Maximum Value: " + maxFloat);
+        System.out.println("Double Minimum Value: " + minDouble);
+        System.out.println("Double Maximum Value: " + maxDouble);
+    }
+}
